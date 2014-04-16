b38e4a22f43a49031a096d443e18778133b5b1d7
Matias Meneses <matiasameneses@gmail.com>
2014-04-16 00:02:27 -0700

    Update my_solution.js

--- week_3/2_js_variables_objects/my_solution.js
+++ week_3/2_js_variables_objects/my_solution.js
@@ -1,41 +1,41 @@
-// I paired [by myself, with:] on this challenge.
+// I completed this challenge by my self.
 
 
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
-
-
+// Create a variable called secretNumber and make is a number equal to 7.
+// Create a variable called password and set it equal to the string "just open 
+// 	the door"
+// Create a variable called allowedIn and make it a false boolean by asking if 
+// 	secretNumber less than zero.
+// Create a variable called members which is an array where the first element 
+// 	is 'John' and the forth element is 'Mary'.
 // __________________________________________
 // Write your code below.
-
-
-
-
-
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = secretNumber<0;
+var members = ['John', 'Emma', 'Matias', 'Mary'];
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
-
-
-
-
-
+// the variable secretNumber test is satisfied by making it a number and specifically the number 7
+// The variable password test is satisfied by making a string and specifically "just open the door"
+// The variable allowedIn test is satisfied by making it a boolean that is false
+// the variable members test is satisfied by making it an array with the first element 'John' and the forth element 'Mary'.
+// the code is concise and the variable names are appropriate to satisfy the test
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
-
-
+// I this was an effective way to understand Test Driven Development.
+// I did not run into many problems, just understanding the concepts at first.
+// While coding Ii did not have any questions, it was straightforward by looking
+// at the ERROR strings what was need to correct the Error. I was having a small
+// issues with writing a correct false boolean statement before actually defining
+// the variable, as well as, forgetting to make the elements of my arrays strings.
+// I a little unclear on how to write my own test for my specific programs but i 
+// think that has much to do with my lack of experience with JavaScript. I enjoyed
+// solving the "puzzle" to create true statements but found it tedious to justify 
+// my original code.
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
 
@@ -101,4 +101,3 @@ assert(
   "The fourth element in the value of members should be 'Mary'.",
   "9. "
 )
-
